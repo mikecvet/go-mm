d561839043f9ee5bb29c769a051b7fc7ddb7614d
Mike Cvet <mike.lp.cvet@gmail.com>
2023-12-04 10:58:32 -0800

    add ratio options

--- plot.py
+++ plot.py
@@ -1,34 +1,42 @@
 import argparse
 import matplotlib.pyplot as plt
 
-def plot_data_from_file(filename, num_columns):
+def plot_data_from_file(filename, mode):
     with open(filename, 'r') as file:
         lines = file.readlines()
 
     # Extract headers
     headers = lines[1].strip().split()
     x_label = headers[0]
-    y_labels = headers[1:num_columns]
 
     # Initialize data storage
     x_data = []
-    y_data = {y_label: [] for y_label in y_labels}
+    y_data = []
 
     # Process data lines
     for line in lines[2:]:
         values = line.strip().split()
         x_data.append(float(values[0]))
-        for i, y_label in enumerate(y_labels):
-            y_data[y_label].append(float(values[i + 1]))
+
+        if mode == 'cmp':
+            y_data.append([float(values[4]) / float(values[1]) * 100, 
+                           float(values[5]) / float(values[2]) * 100])
 
     # Plotting
     plt.figure(figsize=(12, 6))
-    for y_label in y_labels:
-        plt.plot(x_data, y_data[y_label], label=y_label)
+    if mode == 'cmp':
+        plt.plot(x_data, [y[0] for y in y_data], label=headers[4] + '/' + headers[1])
+        plt.plot(x_data, [y[1] for y in y_data], label=headers[5] + '/' + headers[2])
+        plt.ylabel('Relative Completion Time Percentage (%)')
+    else:
+        # Original plotting logic (as per your script)
+        pass
 
-    plt.xlabel('Matrix Cardinality')
-    plt.ylabel('Time in ms')
-    plt.title('Matrix Multiplication Performance Comparison')
+    plt.xlabel(x_label)
+    if mode == 'cmp':
+        plt.title('Performance Ratio - Hand-Written Go to Metal')
+    else:    
+        plt.title('Performance Comparison')
     plt.legend()
     plt.grid(True)
     plt.show()
@@ -36,18 +44,12 @@ def plot_data_from_file(filename, num_columns):
 def main():
     parser = argparse.ArgumentParser(description='Plot graph from data file.')
     parser.add_argument('filename', type=str, help='The data file to plot.')
-    parser.add_argument('--fast', action='store_true', help='Use 5 columns of data instead of 8.')
-    parser.add_argument('--gpuonly', action='store_true', help='Use 3 columns of data instead of 8.')
+    parser.add_argument('--cmp', action='store_true', help='Compare hand-written CPU to GPU methods')
     args = parser.parse_args()
 
-    # Determine the number of columns based on arguments
-    num_columns = 9
-    if args.fast:
-        num_columns = 6
-    elif args.gpuonly:
-        num_columns = 4
+    mode = 'cmp' if args.cmp else 'original'
 
-    plot_data_from_file(args.filename, num_columns)
+    plot_data_from_file(args.filename, mode)
 
 if __name__ == "__main__":
     main()
